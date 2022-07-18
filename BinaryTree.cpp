3a8c404dfc573e535cd6c37b20c415193ad414f2
prernask <91388114+prernask@users.noreply.github.com>
2022-07-18 23:15:30 +0530

    Add files via upload

---
+++ BinaryTree.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+void preorder(struct Node* root){
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+void inorder(struct Node* root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+void postorder(struct Node* root){
+    if(root==NULL){
+        return;
+
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+int main(){
+    struct Node* root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+
+    cout<<"\nPreorder tree"<<"->";
+    preorder(root);
+    cout<<"\nPostorder tree"<<"->"<<endl;
+
+    postorder(root);
+    cout<<"\ninorder tree"<<"->"<<endl;
+
+    inorder(root);
+
+
+
+    return 0;
+}
